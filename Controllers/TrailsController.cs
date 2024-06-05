f4191298a5c62ff6a14576d44316bcb647862326
Martaxxxx <148713900+Martaxxxx@users.noreply.github.com>
2024-06-05 21:51:47 +0200

    Add files via upload

---
+++ Controllers/TrailsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatryExplorer.Models;
+using TatryExplorer.Controllers;
+using System.Threading.Tasks;
+
+
+namespace TatryExplorer.Controllers
+{ 
+
+
+    public class TrailsController : Controller
+    {
+        private object results;
+
+        // Akcja Index, która renderuje widok listy tras
+        public IActionResult Index()
+        {
+            // Tutaj możemy pobrać listę tras z bazy danych lub utworzyć przykładowe dane
+            var trails = new List<Trail>
+            {
+                new Trail { Id = 1, Name = "Trail A", LengthKm = 10, Duration = TimeSpan.FromHours(3), DifficultyLevel = "Easy", MapLink = "https://example.com/map_a", Description = "Description of trail A...", Photo = "path_to_photo_a.jpg" },
+                new Trail { Id = 2, Name = "Trail B", LengthKm = 15, Duration = TimeSpan.FromHours(4), DifficultyLevel = "Medium", MapLink = "https://example.com/map_b", Description = "Description of trail B...", Photo = "path_to_photo_b.jpg" },
+                new Trail { Id = 3, Name = "Trail C", LengthKm = 20, Duration = TimeSpan.FromHours(5), DifficultyLevel = "Difficult", MapLink = "https://example.com/map_c", Description = "Description of trail C...", Photo = "path_to_photo_c.jpg" }
+            };
+
+            return View(trails);
+
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        // Akcja do obsługi wyszukiwania tras
+        [HttpPost]
+      
+
+        private IActionResult View(string v, object results)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Models/Trail.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TatryExplorer.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Nazwa Trasy")]
+        public double LengthKm { get; set; }
+        [Display(Name = "Długość trasy")]
+        public TimeSpan TimeToComplete { get; set; }
+        [Display(Name = "Czas ukończenia")]
+        public TimeSpan Duration { get; set; }
+        public string DifficultyLevel { get; set; }
+        [Display(Name = "Poziom trudności")]
+        public string MapLink { get; set; }
+        [Display(Name = "Zobacz na mapie")]
+        public string Description { get; set; }
+        [Display(Name = "Opis")]
+        public string Image { get; set; }
+        [Display(Name = "Zdjęcie")]
+
+        public string Photo { get; set; }
+    }
+}
